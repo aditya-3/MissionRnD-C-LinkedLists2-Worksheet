ae9a23d6cebd1cbea8101dd4eb5925660b44758f
aditya-3 <aditya-3@missionrnd.com>
2016-03-19 08:54:14 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -10,14 +10,41 @@ ERROR CASES: Return NULL for error cases.
 
 NOTES:
 */
-
+#include<stdlib.h>
 #include <stdio.h>
 
 struct node {
 	int num;
 	struct node *next;
 };
+void link(struct node *temp1, struct node *temp2)
+{
+	struct node *temp=(struct node *)malloc(sizeof(struct node));
+	struct node *t;
+	t = temp1;
+	temp1->next = temp2;
 
+}
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	struct node *result;
+	if (head1 == NULL)
+		return head2;
+	else if (head2 == NULL)
+		return head1;
+	else
+	{
+		if (head1->num < head2->num)
+		{
+			result = head1;
+			result->next = merge2LinkedLists(head1->next,head2);
+		}
+		else
+		{
+			result = head2;
+			result->next = merge2LinkedLists(head1, head2->next);
+		}
+		
+	}
+	return result;
+
 }
